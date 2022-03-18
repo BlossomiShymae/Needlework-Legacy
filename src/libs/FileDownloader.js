ad24c80e4b956810457657028bc72026891e15e8
MissUwuieTime <87099578+MissUwuieTime@users.noreply.github.com>
2022-03-18 00:00:09 -0500

    Add rate limiting for downloading files from apis

--- src/libs/FileDownloader.js
+++ src/libs/FileDownloader.js
@@ -1,12 +1,36 @@
 const axios = require("axios");
+import rateLimit from "axios-rate-limit";
 
 export default class FileDownloader {
+  static hostMap = new Map();
+
   constructor(url, responseType) {
-    this.axios = axios.create({
-      timeout: 5000,
-    });
+    if (!this.isInHostMap(url)) {
+      this.axios = rateLimit(axios.create(), {
+        maxRequests: 5,
+        perMilliseconds: 1000,
+        maxRPS: 5,
+      });
+
+      const formattedURL = new URL(url);
+      FileDownloader.hostMap.set(formattedURL.hostname, this.axios);
+      console.log(
+        `FileDownloader: Added ${formattedURL.hostname} to active host map with unique axios instance.`
+      );
+      console.log(`FileDownloader: Active host map:`);
+      console.log(FileDownloader.hostMap.keys());
+    }
+
+    const formattedURL = new URL(url);
+
+    this.axios = FileDownloader.hostMap.get(formattedURL.hostname, this.axios);
     this.url = url;
     this.responseType = responseType;
+  }
+
+  isInHostMap(url) {
+    const _url = new URL(url);
+    return FileDownloader.hostMap.get(_url.hostname);
   }
 
   async download() {
