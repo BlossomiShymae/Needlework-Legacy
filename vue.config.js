cd3fa6d018930ed03e5293c9937a2c0947d5f263
MissUwuieTime <87099578+MissUwuieTime@users.noreply.github.com>
2022-03-28 18:44:16 -0500

    Add electronBuilder file watchers for main process hot reloading

--- vue.config.js
+++ vue.config.js
@@ -24,6 +24,16 @@ module.exports = {
             ],
           });
       },
+      // Provide an array of files that, when changed, will recompile the main process and restart Electron
+      // Your main process file will be added by default
+      mainProcessWatch: [
+        "src/apis",
+        "src/config",
+        "src/libs",
+        "src/services",
+        "src/static",
+        "src/themes",
+      ],
     },
   },
 };
