24f17f3a58f42b00b84a41655c03e76c2416ac9c
MissUwuieTime <87099578+MissUwuieTime@users.noreply.github.com>
2022-04-20 03:17:58 -0500

    Add rarity and shard type badges

--- src/components/loots/SkinCard.vue
+++ src/components/loots/SkinCard.vue
@@ -1,20 +1,37 @@
 <template>
   <div id="skin-card">
     <Suspense>
-      <BaseLootCard
-        :tileIconPath="skin.tilePath"
-        :name="skin.itemDesc"
-        :loot-name="skin.lootName"
-        :type="skin.type"
-        :count="skin.count"
-        :can-open="true"
-      />
+      <template #default>
+        <div class="default-wrapper">
+          <BaseLootCard
+            :tileIconPath="skin.tilePath"
+            :name="skin.itemDesc"
+            :loot-name="skin.lootName"
+            :type="skin.type"
+            :count="skin.count"
+            :can-open="true"
+            :class="{
+              'skin-shard': skin.type === 'Skin Shard',
+              'skin-permanent': skin.type === 'Skin Permanent',
+            }"
+          />
+          <div class="skin-rarity-epic skin-rarity" v-if="isEpic"></div>
+          <div
+            class="skin-rarity-legendary skin-rarity"
+            v-if="isLegendary"
+          ></div>
+          <div class="skin-rarity-mythic skin-rarity" v-if="isMythic"></div>
+          <div class="skin-rarity-ultimate skin-rarity" v-if="isUltimate"></div>
+        </div>
+      </template>
+      <template #fallback> Loading... </template>
     </Suspense>
   </div>
 </template>
 
 <script>
 import BaseLootCard from "@/components/loots/BaseLootCard";
+import { onMounted } from "@vue/runtime-core";
 
 export default {
   name: "SkinCard",
@@ -27,8 +44,65 @@ export default {
       required: true,
     },
   },
+  data() {
+    return {
+      isEpic: false,
+      isLegendary: false,
+      isMythic: false,
+      isUltimate: false,
+    };
+  },
+  setup(props) {
+    onMounted(() => {
+      console.log(props.skin);
+    });
+  },
+  mounted() {
+    if (this.skin?.rarity?.includes("EPIC")) this.isEpic = true;
+    if (this.skin?.rarity?.includes("LEGENDARY")) this.isLegendary = true;
+    if (this.skin?.rarity?.includes("MYTHIC")) this.isMythic = true;
+    if (this.skin?.rarity?.includes("ULTIMATE")) this.isUltimate = true;
+  },
 };
 </script>
 
-<style>
+<style lang="scss">
+.skin-shard {
+  filter: grayscale(0.875);
+}
+</style>
+
+<style lang="scss" scoped>
+$badge-width: 12px;
+$badge-height: 12px;
+
+.skin-rarity {
+  position: relative;
+  top: -$base-loot-card-height;
+  right: calc(-#{$base-loot-card-width} - 4px);
+  width: $badge-width;
+  height: $badge-height;
+  z-index: 99;
+  background-color: black;
+}
+
+.skin-rarity-epic {
+  clip-path: polygon(50% 0%, 0% 100%, 100% 100%);
+  background-color: #0dd;
+}
+
+.skin-rarity-legendary {
+  clip-path: polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%);
+  background-color: red;
+}
+
+.skin-rarity-mythic {
+  clip-path: polygon(50% 0%, 100% 38%, 82% 100%, 18% 100%, 0% 38%);
+  background-color: purple;
+}
+
+.skin-rarity-ultimate {
+  clip-path: polygon(50% 0, 100% 25%, 100% 75%, 50% 100%, 0 75%, 0 25%);
+  background-color: gold;
+}
 </style>
