40ab59297e1557a11d15838f2bd451271c5019df
MissUwuieTime <87099578+MissUwuieTime@users.noreply.github.com>
2022-04-21 00:21:46 -0500

    Add next level awards

--- src/components/SummonerHorizontalCard.vue
+++ src/components/SummonerHorizontalCard.vue
@@ -18,7 +18,7 @@
     </div>
     <div id="summoner-next-level-info">
       <p class="text-upper text-bold title5">Next Reward</p>
-      <p class="body">Next milestone awards go here</p>
+      <p class="body milestone-awards">{{ milestoneAwards }}</p>
     </div>
     <w-progress
       id="summoner-progress"
@@ -78,12 +78,96 @@ const experienceProgress = computed(() => {
   );
 });
 
+const milestoneAwards = ref("");
+const setMilestoneAwards = (currentSummoner) => {
+  currentSummoner.value.summonerLevel = 549;
+  milestoneAwards.value = "";
+  const levelRewardMap = {
+    2: "450 BE\nPick one: Lux, Master Yi, Miss Fortune, Brand, Darius",
+    3: "450 BE",
+    4: "450 BE",
+    5: "450 BE",
+    6: "900 BE",
+    7: "Poro Pal emote\n900 BE",
+    8: "Random Ward Skin permanent\n900 BE",
+    9: "900 BE",
+    10: "New Rune Page\n900 BE",
+    11: "Random 1350 champion shard, 130 BE",
+    12: "450 BE",
+    13: "450 BE",
+    14: "500 BE",
+    15: "Random 4800 champion shard",
+    16: "500 BE",
+    17: "500 BE",
+    18: "Two random 1350 champion shards",
+    19: "600 BE",
+    20: "Random 4800 champion shard\nRandom icon permanent",
+    21: "600 BE",
+    22: "Random 3150 champion shard, 20 BE",
+    23: "725 BE",
+    24: "725 BE",
+    25: "Random 4800 champion shard\nRandom ward skin permanent",
+    26: "725 BE",
+    27: "Random 450 champion shard\nRandom 3150 champion shard",
+    28: "725 BE",
+    29: "Random 450 champion shard\nRandom 3150 champion shard",
+    30: "Glorious Champion Capsule",
+    40: "Legend 1 Emote",
+    50: "Legend 2 Emote\nRandom summoner icon or ward skin",
+    75: "Legend 3 Emote\nRandom summoner icon or ward skin",
+    100: "Legend 4 Emote\nGlorious Legend ward skin",
+    125: "Legend 5 Emote",
+    150: "Legend 6 Emote",
+    175: "Legend 7 Emote",
+    200: "Legend 8 Emote",
+    225: "Legend 9 Emote",
+    250: "Legend 10 Emote",
+    275: "Legend 11 Emote",
+    300: "Legend 12 Emote",
+    325: "Legend 13 Emote",
+    350: "Legend 14 Emote",
+    375: "Legend 15 Emote",
+    400: "Legend 16 Emote",
+    425: "Legend 17 Emote",
+    450: "Legend 18 Emote",
+    475: "Legend 19 Emote",
+    500: "Legend 20 Emote",
+  };
+
+  milestoneAwards.value +=
+    levelRewardMap[currentSummoner.value.summonerLevel + 1] ?? "";
+
+  if (currentSummoner.value.summonerLevel + 1 > 30) {
+    // Milestone awards always have a Glorious Capsule
+    if (milestoneAwards.value !== "") {
+      milestoneAwards.value += "\nGlorious Champion Capsule";
+    } else {
+      // Non-milestone awards
+      if ((currentSummoner.value.summonerLevel + 1) % 10 == 0) {
+        milestoneAwards.value += "Glorious Champion Capsule";
+      } else if ((currentSummoner.value.summonerLevel + 1) % 1 == 0) {
+        milestoneAwards.value += "Champion Capsule";
+      }
+    }
+
+    // Mythic Essence
+    if (
+      currentSummoner.value.summonerLevel + 1 >= 150 &&
+      (currentSummoner.value.summonerLevel + 1) % 50 == 0
+    ) {
+      milestoneAwards.value += "\n10 Mythic Essence";
+    }
+  }
+};
+setMilestoneAwards(currentSummoner);
+
 window.ipcRenderer.receive("needlework-update", async (uri) => {
   if (uri === routes.CURRENT_SUMMONER || uri === routes.LOL_LOOT_READY) {
     currentSummoner.value = await window.ipcRenderer.invoke("current-summoner");
     profileIcon.value = await (
       await useProfileIcon(currentSummoner)
     ).profileIcon.value;
+    setMilestoneAwards(currentSummoner);
     forceRerender(componentKey);
   }
 });
@@ -143,6 +227,12 @@ window.ipcRenderer.receive("needlework-update", async (uri) => {
     grid-area: sp;
   }
 }
+
+.milestone-awards {
+  white-space: pre-wrap;
+  word-wrap: break-word;
+}
+
 .floating {
   animation-name: floating;
   animation-duration: 6s;
