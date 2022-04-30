1305d840a79194568b8e44817a9a3c26d275e782
MissUwuieTime <87099578+MissUwuieTime@users.noreply.github.com>
2022-04-30 18:01:19 -0500

    Add comments

--- src/channels.ts
+++ src/channels.ts
@@ -1,7 +1,19 @@
+/**
+ * An enum used for `ipcRenderer.send`.
+ * ```
+ * window.ipcRenderer.send(SChannel.example, data: any);
+ * ```
+ */
 export enum SChannel {
   empty = "",
 }
 
+/**
+ * An enum used for asynchronous `ipcRenderer.invoke`.
+ * ```
+ * await window.ipcRenderer.invoke(IChannel.wallet);
+ * ```
+ */
 export enum IChannel {
   currentSummoner = "current-summoner",
   wallet = "wallet",
@@ -17,10 +29,22 @@ export enum IChannel {
   setStore = "app-set-store",
 }
 
+/**
+ * An enum used for `ipcRenderer.receive`.
+ * ```
+ * window.ipcRenderer.receive(RChannel.needleworkUpdate, (): any => void);
+ * ```
+ */
 export enum RChannel {
   needleworkUpdate = "needlework-update",
 }
 
+/**
+ * An enum used for `ipcRenderer.once`.
+ * ```
+ * window.ipcRenderer.once(RChannel.example, (): any => void);
+ * ```
+ */
 export enum OChannel {
   empty = "",
 }
