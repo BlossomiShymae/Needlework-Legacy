a448205f9aeac6fe792fc1438b7e0f2653d53556
MissUwuieTime <87099578+MissUwuieTime@users.noreply.github.com>
2022-05-05 18:37:45 -0500

    Use enum type instead of hard-coded strings

--- src/stores/loot.ts
+++ src/stores/loot.ts
@@ -3,12 +3,10 @@ import type { PlayerLoot } from "@/types/PlayerLoot";
 import { Loot } from "@/enums/loot";
 import { PlayerLootMap } from "@/types/PlayerLootMap";
 
-
 interface State {
-  playerLootMap: PlayerLootMap,
-  lootTable: object,
+  playerLootMap: PlayerLootMap;
+  lootTable: object;
 }
-
 
 function filterMapByCategory(
   playerLootMap: PlayerLootMap,
@@ -18,55 +16,58 @@ function filterMapByCategory(
   return array.filter((loot) => loot.displayCategories === category);
 }
 
-export const useLootStore = defineStore('loot', {
+export const useLootStore = defineStore("loot", {
   state: (): State => ({
     playerLootMap: new Map<string, PlayerLoot>(),
     lootTable: {},
   }),
   getters: {
-    championCapsules: (state) => state.playerLootMap.get("CHEST_128"),
-    gloriousChampionCapsules: (state) => state.playerLootMap.get("CHEST_129"),
-    lootChampions: (state) => filterMapByCategory(
-      state.playerLootMap, Loot.DisplayCategories.CHAMPION
-    ),
-    lootEternals: (state) => filterMapByCategory(
-      state.playerLootMap, Loot.DisplayCategories.ETERNALS
-    ),
-    lootChests: (state) => filterMapByCategory(
-      state.playerLootMap, Loot.DisplayCategories.CHEST
-    ),
-    lootEmotes: (state) => filterMapByCategory(
-      state.playerLootMap, Loot.DisplayCategories.EMOTE
-    ),
-    lootSkins: (state) => filterMapByCategory(
-      state.playerLootMap, Loot.DisplayCategories.SKIN
-    ),
-    lootIcons: (state) => filterMapByCategory(
-      state.playerLootMap, Loot.DisplayCategories.SUMMONER_ICON
-    ),
-    lootWardSkins: (state) => filterMapByCategory(
-      state.playerLootMap, Loot.DisplayCategories.WARD_SKIN
-    ),
-    lootTacticians: (state) => filterMapByCategory(
-      state.playerLootMap, Loot.DisplayCategories.COMPANION
-    ),
-    lootOthers: (state) => filterMapByCategory(
-      state.playerLootMap, Loot.DisplayCategories.OTHER
-    ),
-    lootUncategorized: (state) => filterMapByCategory(
-      state.playerLootMap, Loot.DisplayCategories.NONE
-    ),
+    championCapsules: (state) =>
+      state.playerLootMap.get(Loot.LootId.CHAMPION_CAPSULE),
+    gloriousChampionCapsules: (state) =>
+      state.playerLootMap.get(Loot.LootId.GLORIOUS_CHAMPION_CAPSULE),
+    gloriousChampionCapsulesMythic: (state) =>
+      state.playerLootMap.get(Loot.LootId.GLORIOUS_CHAMPION_CAPSULE_MYTHIC),
+    lootChampions: (state) =>
+      filterMapByCategory(state.playerLootMap, Loot.DisplayCategories.CHAMPION),
+    lootEternals: (state) =>
+      filterMapByCategory(state.playerLootMap, Loot.DisplayCategories.ETERNALS),
+    lootChests: (state) =>
+      filterMapByCategory(state.playerLootMap, Loot.DisplayCategories.CHEST),
+    lootEmotes: (state) =>
+      filterMapByCategory(state.playerLootMap, Loot.DisplayCategories.EMOTE),
+    lootSkins: (state) =>
+      filterMapByCategory(state.playerLootMap, Loot.DisplayCategories.SKIN),
+    lootIcons: (state) =>
+      filterMapByCategory(
+        state.playerLootMap,
+        Loot.DisplayCategories.SUMMONER_ICON
+      ),
+    lootWardSkins: (state) =>
+      filterMapByCategory(
+        state.playerLootMap,
+        Loot.DisplayCategories.WARD_SKIN
+      ),
+    lootTacticians: (state) =>
+      filterMapByCategory(
+        state.playerLootMap,
+        Loot.DisplayCategories.COMPANION
+      ),
+    lootOthers: (state) =>
+      filterMapByCategory(state.playerLootMap, Loot.DisplayCategories.OTHER),
+    lootUncategorized: (state) =>
+      filterMapByCategory(state.playerLootMap, Loot.DisplayCategories.NONE),
     orangeEssence: (state) => state.playerLootMap.get("CURRENCY_cosmetic"),
     mythicEssence: (state) => state.playerLootMap.get("CURRENCY_mythic"),
     keys: (state) => state.playerLootMap.get("MATERIAL_key"),
   },
   actions: {
-    updatePlayerLootMap (jsonMap: object) {
+    updatePlayerLootMap(jsonMap: object) {
       const entries = Object.entries(jsonMap);
       this.playerLootMap = new Map(entries) as PlayerLootMap;
     },
     setLootTable(value: object) {
       this.lootTable = value;
-    }
-  }
-})
+    },
+  },
+});
